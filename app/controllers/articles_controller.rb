5ba2f6d2d564d20a28bb0f1724d0967eda8e30da
StevenScib <x23174668@student.ncirl.ie>
2024-05-03 14:53:11 +0100

    Added CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,40 @@
 class ArticlesController < ApplicationController
+    
+def index
+  @articles = Article.all
+  render json: @articles
+end
+
+def show
+  @article = Article.find(params[:id])
+  render json: @article
+end
+
+def create
+  @article = Article.new(article_params)
+  if @article.save
+    render json: @article, status: :created
+  else
+    render json: @article.errors, status: :unprocessable_entity
+  end
+end
+
+private
+def article_params
+  params.require(:article).permit(:title, :body, :published)
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    render json: @article
+  else
+    render json: @article.errors, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_30_135729) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "published"
+    t.boolean "published", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
