cabbfd041293429df0a3ba7530bc8f8b23c897ae
StevenScib <x23174668@student.ncirl.ie>
2024-05-04 20:08:19 +0100

    added cors, modified controller to let react app work, added article profiles so that the react can pull from it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,40 +1,47 @@
 class ArticlesController < ApplicationController
-    
-def index
-  @articles = Article.all
-  render json: @articles
-end
-
-def show
-  @article = Article.find(params[:id])
-  render json: @article
-end
-
-def create
-  @article = Article.new(article_params)
-  if @article.save
-    render json: @article, status: :created
-  else
-    render json: @article.errors, status: :unprocessable_entity
+  skip_before_action :verify_authenticity_token, only: [:destroy]
+  skip_before_action :verify_authenticity_token, only: [:update]
+  skip_before_action :verify_authenticity_token, only: [:create]
+  
+  def index
+    @articles = Article.all
+    render json: @articles
   end
-end
 
-private
-def article_params
-  params.require(:article).permit(:title, :body, :published)
-end
-
-def update
-  @article = Article.find(params[:id])
-  if @article.update(article_params)
+  def show
+    @article = Article.find(params[:id])
     render json: @article
-  else
-    render json: @article.errors, status: :unprocessable_entity
   end
-end
 
-def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
-end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      render json: { message: 'Article deleted successfully' }, status: :ok
+    else
+      render json: { error: 'Failed to delete article' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :published)
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
-  
-      resource '/articles',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options],
-        credentials: true
+      origins 'http://localhost:3001' # Replace this with your React app's URL
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
     end
   end
