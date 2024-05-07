5d99a6c84fb86e7ae0be0dd2ecec79f5265191cf
StevenScib <x23174668@student.ncirl.ie>
2024-05-07 15:02:27 +0100

    added config.yml

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001' # Replace this with your React app's URL
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    end
+  allow do
+    origins '*'  # Allows all origins
+    resource '*',  # Allows all resources
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options, :head]  # Allows all methods
   end
+end
